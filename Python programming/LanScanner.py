a4dca017318fb1ad5b4e39b872c25b2c16c63763
Houwenda <houwd123@126.com>
2018-05-02 00:24:37 +0800

    git stage

--- Python programming/LanScanner.py
+++ Python programming/LanScanner.py
@@ -2,18 +2,20 @@
 
 import thread
 import time
-from subprocess import Popen,PIPE
+import re
+import os
 def ip_check(ip):
-    check = Popen(['cmd.exe','ping -n 2'+ip],stdin=PIPE,stdout=PIPE)
-    data = check.stdout.read()
-    if 'ttl' in data:
+    command = 'ping -n 1 '+ ip
+    check = os.popen(command,'r',-1)
+    data = check.read()
+    if 'TTL' in data:
         print '%s is up'%ip
 
 def main():
     for i in range(1,255):
         ip = '192.168.1.' + str(i)
         thread.start_new_thread(ip_check, (ip,))
-        time.sleep(1)
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
